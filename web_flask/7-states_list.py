230bc3032711002f3a3805d06dff268e16867f70
Vozudo <jploperatobon@gmail.com>
2023-02-24 11:43:12 -0500

    Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """display a HTML with the states of the db"""
+    states = storage.all(State)
+    return render_template(
+        '7-states_list.html',
+        states=sorted(
+            states.values(),
+            key=lambda x: x.name))
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
