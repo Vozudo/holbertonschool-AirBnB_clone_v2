8ccc6707ccec53f8eff4311dc3da280a01928262
Vozudo <jploperatobon@gmail.com>
2023-02-24 09:49:09 -0500

    Task 3 & 4

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>',)
+def c_is_fun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
